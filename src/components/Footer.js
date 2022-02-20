48b9617578e90d86ddbe5c8532a7bbf977593c0b
Guido LM <guido99lm@gmail.com>
2022-02-20 16:56:20 -0300

    Desafío clase 3

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>&copy;copyright 2022 - Guido Latella Minniti</p>
+        <p>Todos los derechos reservados</p>
+    </footer>
+  )
+}
+export default Footer
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//rafce short-cut
+const NavBar = () => {
+  return (
+    <header>
+        <h1 className='titulo'>E-commerce</h1>
+        <a href='#'>Inicio</a><a href='#'>Tienda</a><a href='#'>Nosotros</a><a href='#'>FAQ</a>
+    </header>
+  )
+}
+
+export default NavBar
